5bcecbe5602c871c3c8cc29a7b1e702db906cff1
701030552 <701030552@2260I-L29-031>
2025-03-28 14:41:23 -0400

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dice_game
+{
+    public class Die
+    {
+        private Random _generator;
+        private int _roll;
+
+        public Die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+
+        }
+        public Die(int roll)
+        {
+            _generator = new Random();
+            _roll = roll;
+        }
+        public int Roll
+
+        {
+            get { return _roll; }
+        }
+        public override string ToString()
+        {
+            return _roll.ToString();
+        }
+
+        public void RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+        }
+
+        public void DrawDie()
+        {
+            if (Roll == 1)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|   |");
+                Console.WriteLine("| o |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("-----");
+            }
+            else if (Roll == 2)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|o  |");
+                Console.WriteLine("|  o|");
+                Console.WriteLine("-----");
+            }
+            else if (Roll == 3)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|  o|");
+                Console.WriteLine("|o  |");
+                Console.WriteLine("|  o|");
+                Console.WriteLine("-----");
+
+            }
+
+            else if (Roll == 4)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|o  o|");
+                Console.WriteLine("|    |");
+                Console.WriteLine("|o  o|");
+                Console.WriteLine("------");
+
+            }
+
+            else if (Roll == 5)
+            {
+
+                Console.WriteLine("-----");
+                Console.WriteLine("|o  o|");
+                Console.WriteLine("| O  |");
+                Console.WriteLine("|o  o|");
+                Console.WriteLine("------");
+
+            }
+            else if (Roll == 6)
+            {
+
+                Console.WriteLine("-----");
+                Console.WriteLine("|o  o|");
+                Console.WriteLine("|o  o|");
+                Console.WriteLine("|o  o|");
+                Console.WriteLine("------");
+
+            }
+
+
+        }
+    }
+}
+     
+    
+
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace dice_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Console.WriteLine("Welcome to the Casino Dice Game");
+            Console.WriteLine();
+            Console.WriteLine("Rules: ");
+            Console.WriteLine("If you bet 'DOUBLES' for $5 -  you'll win $10 ");
+            Console.WriteLine("If you bet 'NOT DOUBLES' for $10 "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
